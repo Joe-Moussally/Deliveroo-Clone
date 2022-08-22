571a140e080162b889edef0ad62f7a9b9ba74cbc
MJoe111 <joemoussally111@gmail.com>
2022-08-22 15:25:37 +0300

    [App] Adding search bar to header

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native'
 import { useLayoutEffect } from 'react';
-import { Image, SafeAreaView, Text, View } from "react-native";
+import { Image, SafeAreaView, Text, TextInput, View } from "react-native";
 import { UserIcon, SearchIcon, AdjustmentsIcon } from 'react-native-heroicons/outline'
 import { ChevronDownIcon } from 'react-native-heroicons/solid'
 
@@ -17,7 +17,7 @@ const HomeScreen = () => {
     return (
         <SafeAreaView className='bg-white pt-3'>
                 {/* Header */}
-                <View className='flex-row pb-3 mt-8 items-center space-x-2'>
+                <View className='flex-row pb-3 mt-8 items-center space-x-2 px-2'>
                     <Image
                         source={{uri:'https://links.papareact.com/wru'}}
                         className='h-7 w-7 bg-gray-300 p-4 rounded-full'
@@ -32,6 +32,19 @@ const HomeScreen = () => {
                     </View>
 
                     <UserIcon size={35} color='#00CCBB'/>
+                </View>
+
+                {/* Search */}
+                <View className='flex-row items-center pb-2 mx-2 space-x-2'>
+                    <View className='flex-row space-x-2 flex-1 bg-gray-200 p-2 rounded'>
+                        <SearchIcon color='#00CCBB'/>
+                        <TextInput
+                            placeholder='Restaurants and cuisines...'
+                            keyboardType='default'
+                        />
+                    </View>
+
+                    <AdjustmentsIcon color='#00CCBB'/>
                 </View>
         </SafeAreaView>
     );

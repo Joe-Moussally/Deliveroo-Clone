a5ed13822ca3630e130c426124c9f8eff94544fd
MJoe111 <joemoussally111@gmail.com>
2022-08-22 15:14:51 +0300

    [App] Adding style to home screen header

--- deliveroo-app/screens/HomeScreen.js
+++ deliveroo-app/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import { useNavigation } from '@react-navigation/native'
 import { useLayoutEffect } from 'react';
-import { Text } from "react-native";
+import { Image, SafeAreaView, Text, View } from "react-native";
+import { UserIcon, SearchIcon, AdjustmentsIcon } from 'react-native-heroicons/outline'
+import { ChevronDownIcon } from 'react-native-heroicons/solid'
 
 const HomeScreen = () => {
 
@@ -13,7 +15,25 @@ const HomeScreen = () => {
     },[])
 
     return (
-        <Text className='text-red-500'>Home Screen</Text>
+        <SafeAreaView className='bg-white pt-3'>
+                {/* Header */}
+                <View className='flex-row pb-3 mt-8 items-center space-x-2'>
+                    <Image
+                        source={{uri:'https://links.papareact.com/wru'}}
+                        className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+                    />
+                
+                    <View className='flex-1'>
+                        <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
+                        <Text className='font-bold text-xl'>
+                            Current Location
+                            <ChevronDownIcon size={20} color='#00CCBB' />
+                        </Text>
+                    </View>
+
+                    <UserIcon size={35} color='#00CCBB'/>
+                </View>
+        </SafeAreaView>
     );
 }
  

6571d65ba85071402a94af985838a80b4753fc46
MJoe111 <joemoussally111@gmail.com>
2022-08-22 20:04:07 +0300

    [App] Adding plus and minus buttons for menu items in restaurant screen

--- deliveroo-app/components/DishRow.js
+++ deliveroo-app/components/DishRow.js
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid'
 
 const DishRow = ({id,name,description,price,image}) => {
+
+  const [isPressed, setIsPressed] = useState(false)
+
   return (
-    <TouchableOpacity className='bg-white border p-4 border-gray-200'>
+    <>
+    <TouchableOpacity
+    className={'bg-white border p-4 border-gray-200'+isPressed?'border-b-0 bg-white':''}
+    onPress={() => setIsPressed(!isPressed)}
+    >
       <View className='flex-row'>
           <View className='flex-1 pr-2'>
             <Text className='text-lg mb-1'>{name}</Text>
@@ -22,6 +31,33 @@ const DishRow = ({id,name,description,price,image}) => {
         </View>
       </View>
     </TouchableOpacity>
+
+    {
+      isPressed && (
+        <View className='bg-white px-4'>
+          <View className='items-center flex-row space-x-2 pb-3'>
+
+            <TouchableOpacity>
+              <MinusCircleIcon
+                size={40}
+                color={'#00CCBB'}
+              />
+            </TouchableOpacity>
+
+            <Text>0</Text>
+
+            <TouchableOpacity>
+              <PlusCircleIcon
+              size={40}
+              color={'#00CCBB'}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )
+    }
+
+    </>
   )
 }
 

953d138d1e634780d3ae5578d6090aea7babe03c
MJoe111 <joemoussally111@gmail.com>
2022-08-22 16:28:31 +0300

    [App] Creating Restaurant card component

--- deliveroo-app/components/Featured.js
+++ deliveroo-app/components/Featured.js
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import RestaurantCard from './RestaurantCard'
 
 const Featured = ({id,title,description}) => {
   return (
@@ -11,6 +12,54 @@ const Featured = ({id,title,description}) => {
         </View>
 
         <Text className='text-xs text-gray-500 px-4'>{description}</Text>
+
+        <ScrollView
+            horizontal
+            contentContainerStyle={{
+                paddingHorizontal:15,
+            }}
+            showsHorizontalScrollIndicator={false}
+            className='pt-4'
+        >
+            {/* Restaurant Cards */}
+            <RestaurantCard
+                id={123}
+                imgUrl='https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+                title='Yolo Sushi'
+                rating={4.5}
+                genre='Japanese'
+                address='123 Main St'
+                short_description='This is a short description'
+                dishes={[]}
+                long={20}
+                lat={0}
+            />
+                        <RestaurantCard
+                id={123}
+                imgUrl='https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+                title='Yolo Sushi'
+                rating={4.5}
+                genre='Japanese'
+                address='123 Main St'
+                short_description='This is a short description'
+                dishes={[]}
+                long={20}
+                lat={0}
+            />
+                        <RestaurantCard
+                id={123}
+                imgUrl='https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+                title='Yolo Sushi'
+                rating={4.5}
+                genre='Japanese'
+                address='123 Main St'
+                short_description='This is a short description'
+                dishes={[]}
+                long={20}
+                lat={0}
+            />
+        </ScrollView>
+
     </View>
   )
 }

81ee7259677e7aaffc5423806b4e6a8be4252b29
MJoe111 <joemoussally111@gmail.com>
2022-08-23 14:44:24 +0300

    [App] Styling header for delivery screen

--- deliveroo-app/screens/DeliveryScreen.js
+++ deliveroo-app/screens/DeliveryScreen.js
@@ -1,9 +1,33 @@
-import { View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useSelector } from 'react-redux'
+import { selectRestaurant } from '../features/restaurantSlice'
+import { XIcon } from 'react-native-heroicons/solid'
 
 const DeliveryScreen = () => {
+    const navigation = useNavigation()
+    const restaurant = useSelector(selectRestaurant)
+
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
+    <View className='bg-[#00CCBB] flex-1'>
+      <SafeAreaView className='z-50'>
+
+        <View className='flex-row justify-between items-center p-5'>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <XIcon color='white' size={30}/>
+            </TouchableOpacity>
+            <Text className='font-light text-white text-lg'>Order Help</Text>
+        </View>
+        
+        <View>
+            <View className='bg-white mx-5 my-2 rounded-md p-6 z-60 shadow-md'>
+                <Text className='text-lg text-gray-400'>Estimated Arrival</Text>
+                <Text className='text-4xl font-bold'>45-55 Minutes</Text>
+            </View>
+        </View>
+
+      </SafeAreaView>
     </View>
   )
 }

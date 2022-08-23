168c147b218c87b1a5a8d1025d5a6bf772e9ea44
MJoe111 <joemoussally111@gmail.com>
2022-08-23 14:15:58 +0300

    [App] Adding gif animation to placing order screen

--- deliveroo-app/screens/PreparingOrderScreen.js
+++ deliveroo-app/screens/PreparingOrderScreen.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
+import * as Animatable from 'react-native-animatable'
 
 const PreparingOrderScreen = () => {
   return (
-    <View>
-      <Text>PreparingOrderScreen</Text>
-    </View>
+    <SafeAreaView className='bg-[#00CCBB] flex-1 justify-center items-center'>
+      <Animatable.Image
+        source={require('../assets/place_order.gif')}
+        animation='slideInUp'
+        iterationCount={1}
+        className='w-80 h-80'
+      />
+    </SafeAreaView>
   )
 }
 

2a1a7de87bcc2484dc699655afc1fca8926462af
MJoe111 <joemoussally111@gmail.com>
2022-08-23 14:48:58 +0300

    [App] Fixing header spacing for delivery screen

--- deliveroo-app/screens/DeliveryScreen.js
+++ deliveroo-app/screens/DeliveryScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useSelector } from 'react-redux'
 import { selectRestaurant } from '../features/restaurantSlice'
@@ -20,11 +20,18 @@ const DeliveryScreen = () => {
             <Text className='font-light text-white text-lg'>Order Help</Text>
         </View>
         
-        <View>
-            <View className='bg-white mx-5 my-2 rounded-md p-6 z-60 shadow-md'>
-                <Text className='text-lg text-gray-400'>Estimated Arrival</Text>
-                <Text className='text-4xl font-bold'>45-55 Minutes</Text>
+        <View className='bg-white mx-5 my-2 rounded-md p-6 z-60 shadow-md'>
+            <View className='flex-row justify-between'>
+                <View>
+                    <Text className='text-lg text-gray-400'>Estimated Arrival</Text>
+                    <Text className='text-3xl font-bold'>45-55 Minutes</Text>
+                </View>
+                <Image
+                    source={{uri:'https://links.papareact.com/fls'}}
+                    className='h-20 w-20'
+                />
             </View>
+
         </View>
 
       </SafeAreaView>
